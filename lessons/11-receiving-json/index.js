28379a205e6ee5a735b435150219d387013707b7
SkyNegar <99282182+SkyNegar@users.noreply.github.com>
2022-02-08 15:11:53 -0600

    Add files via upload

--- lessons/11-receiving-json/index.js
+++ lessons/11-receiving-json/index.js
@@ -1,18 +1,15 @@
 const skyciv = require('skyciv');
 
 const apiObject = {
-	auth: {
-		username: 'YOUR_SKYCIV_USERNAME',
-		key: 'YOUR_SKYCIV_API_KEY',
-	},
+	auth: { "username": "YOUR_SKYCIV_USERNAME", "key": "OUR_SKYCIV_API_KEY" },
 	functions: [
 		{
 			function: 'S3D.session.start',
 			arguments: {
 				keep_open: false,
-			},
-		},
-	],
+			}
+		}
+	]
 };
 
 const callback = function (res) {
